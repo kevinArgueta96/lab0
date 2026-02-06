3994ffd971c63306d0c719399af60a88f32452cf
kargueta <kargueta@osigu.com>
2026-02-05 21:57:05 -0600

    Add first solutions

--- buyLotsOfFruit.py
+++ buyLotsOfFruit.py
@@ -27,20 +27,30 @@ from __future__ import print_function
 fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
                'limes': 0.75, 'strawberries': 1.00}
 
-
 def buyLotsOfFruit(orderList):
     """
         orderList: List of (fruit, numPounds) tuples
 
     Returns cost of order
     """
+    print("orderList= "+ str(orderList) +"  fruitPrices" +str(fruitPrices))
     totalCost = 0.0
-    "*** YOUR CODE HERE ***"
+    # orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+
+    for fruit, total in orderList:
+        if fruit not in fruitPrices:
+            print("Fruit "+str(fruit)+ "not found")
+            return None
+
+        totalCost += total * fruitPrices[fruit]
+
     return totalCost
+
 
 
 # Main Method
 if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
     orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+    #listWithFieldsForError = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0),('new_fruit', 5.1)]
     print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
