bbb82128dec0ab2aa1a9ed10654c5abed38cb327
kargueta <kargueta@osigu.com>
2026-02-07 13:16:46 -0600

    Solve final problem

--- shopSmart.py
+++ shopSmart.py
@@ -29,18 +29,28 @@ def shopSmart(orderList, fruitShops):
         orderList: List of (fruit, numPound) tuples
         fruitShops: List of FruitShops
     """
-    "*** YOUR CODE HERE ***"
-    return None
+
+    totalsPrices = []
+    for shop in fruitShops:
+        totalsPrices.append((shop, shop.getPriceOfOrder(orderList)))
+
+    bestBuy, bestPrice = min(totalsPrices, key=lambda x: x[1])
+
+    return bestBuy
 
 
 if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
     orders = [('apples', 1.0), ('oranges', 3.0)]
+
     dir1 = {'apples': 2.0, 'oranges': 1.0}
-    shop1 = shop.FruitShop('shop1', dir1)
     dir2 = {'apples': 1.0, 'oranges': 5.0}
+
+    shop1 = shop.FruitShop('shop1', dir1)
     shop2 = shop.FruitShop('shop2', dir2)
+
     shops = [shop1, shop2]
+
     print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
     orders = [('apples', 3.0)]
     print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
